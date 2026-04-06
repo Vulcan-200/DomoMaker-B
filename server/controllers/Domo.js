84564ab86e4498df68bb19a9713f5030ca20f237
Mark Starovoytov <70524900+Vulcan-200@users.noreply.github.com>
2026-04-05 23:32:13 -0400

    fixed res.render

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -6,7 +6,7 @@ const makerPage = async (req, res) => {
         const query = {owner: req.session.account._id};
         const docs = await Domo.find(query).select('name age').lean().exec();
         
-        res.render('app');
+        res.render('app', {domos: docs});
     } catch (err) {
         console.log(err);
         return res.status(500).json({ error: 'Error retrieving domos!'});
